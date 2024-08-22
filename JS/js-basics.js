8eec7c14c9b615406799f3708e4206803558b4df
justin-swenson111 <justinswenson111@gmail.com>
2024-08-21 18:22:25 -0700

    First Repo notes
    Year 1 python and JS notes

---
+++ JS/js-basics.js
@@ -0,0 +1,84 @@
+//console.log testing
+let Name = "Justin"
+let favfoods = ["Mexican", "German", "Italian"]
+let age = 16
+//let alive= true
+console.log(Name+" " +age+" "+" "+favfoods)
+//Multiline comments testing
+let message = ` This is a multi-line
+message using backtics`
+console.log(message)
+// const
+//variable that cannot be changed after its creation
+const lastName= "Swenson"
+let result= "hired"
+console.log("The person we interviewed name was: "+ lastName+ ". He was: "+result+".")
+//concatenation testting
+let integer =1
+let string = " 3 "
+let boolean = true
+console.log(integer+string+boolean)
+console.log(integer-string)
+//if statements
+let age1=23, age2=56
+if (age1 > 45){
+    console.log("they old")
+}
+else if (age1<30){
+    console.log("babies am i right")
+}
+else{
+    console.log("they are getting there")
+}
+let bf="Eli"
+let thinksBF="AJ"
+    if (bf!=thinksBF){
+        console.log("You need more friends")
+    }
+    if (15=="15"){
+        console.log("You can read")
+    }
+    //== converts type first then compares
+    if ("15"==="15"){
+        console.log("You can read")
+    }
+    //=== does not convert first
+let alive=true
+let year="2024"
+let number= 7
+if (alive==true && year==2024){
+console.log("yay")
+}
+// or you could use nested
+if (alive==true){
+    if (year==2024){
+        console.log("yay")
+    }
+}
+//can get rid of ==true for boolean statements since they are already true or false
+if (alive){
+    console.log("alive")
+}
+let money=175
+if (money>=0 && money<=20){
+    console.log("Broke Netflix")
+}
+else if (money>=21 && money<=60){
+    console.log("Movie Theater")
+}
+else if (money>=61 && money<=80){
+    console.log("Dinner")
+}
+else{
+    console.log("Sky diving")
+}
+
+let input1="1"
+let input2=1
+
+console.log("comparing " +input1+ "("+typeof(input1)+") and " + input2 + "(" + typeof(input2)+")") 
+console.log('When compared with == is'+(input1==input2))
+console.log('When compared with === is ' +(input1===input2))
+console.log('When compared with Object.is is '+ Object.is(input1,input2))
+
+
---
+++ Python/python-basics.py
@@ -0,0 +1,71 @@
+#print testing
+import math
+school="west-mec"
+first_name = "Justin"
+last_name = "Swenson"
+print(first_name, last_name)
+#testing type command
+print(type(school))
+#multilane string test
+message = """ This is a multi lane
+message using triple quotes"""
+print(message)
+#testing string conversion
+name="justin"
+age=16
+print(name+" "+str(age))
+#testing variable conversion
+integer=1
+string=" 3 "
+boolean= True
+print(str(integer)+string+str(boolean))
+print(integer-int(string))
+print(str(True))
+#if statements
+age1=23
+age2=56
+if age2 > 45:
+    print("they old")
+elif age2<30:
+    print("Babies am I right")
+else:
+    print("they are getting there")
+bf="ELi"
+thinks_bf="AJ"
+#!= means is not equal to
+if bf != thinks_bf:
+    print("You have friends")
+if 15==15:
+    print("You can read")
+#python doesnt automatically convert variables
+if 15=="15":
+    print("No")
+#need to convert manually for comparisons
+if 15==int("15"):
+    print("good conversion")
+alive=True
+year="2024"
+number=7
+if alive==True and year==str(2024):
+    print("yay")
+#you could also do nested
+if alive==True:
+    if year==str(2024):
+        print("yay")
+money=140
+if money>=0 and money<=20:
+    print("Broke Netiflix")
+elif money>=21 and money<=60:
+    print("Movie Theater")
+elif money>=61 and money<=80:
+    print("Dinner")
+else:
+    print("Sky diving")
+#first_name_input_test = input("what is your first name ")
+#print("your name is "+ first_name_input_test)
+
+
+a= float(input("what is the side a of the triangle"))
+b= float(input("what is the side b of the triangle"))
+hytonenuse= math.sqrt(a*a + b*b)
+print("the hypotenuse is " + str(hytonenuse))
