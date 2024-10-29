7b344289407d94a1a795c2ad7ea6bd875cca1887
justin-swenson111 <justinswenson111@gmail.com>
2024-10-28 18:19:30 -0700

    stuff

--- Notes/JS/js-basics.js
+++ Notes/JS/js-basics.js
@@ -133,10 +133,8 @@ let ageTest=20
 let canDrink= (ageTest>=21) ? true: false;
 console.log(canDrink)
 
-let a =1
-let b = 4
-let check = (a+b<4) ?"result":"over";
-console.log(check)
+// let a =1
+// let b = 4
 
 let grade=90
 let letterGrade
@@ -240,4 +238,32 @@ outer: for (let i =0; i<3; i++){
     }
 }
 alert("done!")
+
+var music =["jazz", "blues"]
+music.push("R&B")
+if (music.length%2==1){
+    music[Math.floor(music.length/2)]="none"
+}
+music.splice(Math.floor(music.length/2),0,"classic")
+music.unshift("rap","acapella")
+console.log(music)
+
+let b = ["Phineas", "Ferb", "Perry", "Doofenschmirtz", "Isabella", "Candace"]
+let c = ["Jake", "Finn", "LSP", "Marceline", "Princess Bubblegum", "Lemonheads"]
+
+let e = ["Beemo", ...c, "Lich", "Flame Princess", ]
+console.log(e)
+
+let f = [...b, ...c]
+console.log(f)
+
+let words = "Who would stop talking first Rohan, Monty, or Justin"
+let newWords=(words.split("i"))
+console.log(newWords[2])
+
+var box1  = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31]
+var box2 = [2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31]
+var box3 = [4,5,6,7,12,13,14,15,20,21,23,24,28,29,30,31]
+var box4 = [8,9,10,11,12,13,14,15,24,25,26,27,28,29,30,31]
+var box5 = [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 
